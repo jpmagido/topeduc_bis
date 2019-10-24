1454533a0deafba052c8702d5212387d8bf01ca4
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-10-24 16:23:04 +0200

    config Routes for Devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions', registrations: 'users/registrations'
+      }
+  
   get 'static/home'
   get 'static/test'
   root 'static#test'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
