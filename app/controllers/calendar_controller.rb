2fad33b1271d8576e5e2576c078fa34a62586154
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-10-25 00:24:23 +0200

    CREATE Calendar controller + views

---
+++ app/controllers/calendar_controller.rb
@@ -0,0 +1,28 @@
+class CalendarController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  	@new_calendar = Calendar.new
+  end
+
+  def create
+  	Calendar.create(params_calendar)
+  end
+
+  def edit
+  	@calendar_to_update = Calendar.find(params[:id])
+  end
+
+  def update
+  	@calendar_to_update = Calendar.find(params[:id])
+  	@calendar_to_update.update(params_calendar)
+  end
+
+  def params_calendar
+  	params.require(:calendar).permit(:user_id, :pace, :starts_at, :duration, :recurring)
+  end
+end
