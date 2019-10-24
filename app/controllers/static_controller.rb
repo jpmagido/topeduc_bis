82e652cad5c7f2b9ea8610b4d3d9015686d27d61
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-10-25 00:20:50 +0200

    Change secret page front

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,6 +6,15 @@ class StaticController < ApplicationController
   end
 
   def secret_page
-  	
+  	@user_email = current_user.email
+  	@user_first_name = current_user.first_name
+  	@user_last_name = current_user.last_name
+  	@user_job = current_user.job
+  	@user_nationality = current_user.nationality
+  	@user_phone = current_user.phone
+  	@user_adress = current_user.adress
+  	@user_zip_code = current_user.ZIP_CODE
+  	@user_availability = current_user.availability
+
   end
 end
