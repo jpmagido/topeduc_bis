6c5e5c1194208e6d4abead58459feaa94a5637df
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-10-24 16:22:40 +0200

    ADD User Devise controller + config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_permitted_parameters
+		attributes = [:first_name, :last_name, :email, :phone, :job, :nationality, :password, :password_confirmation, :availability]
+		devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(attributes)}
+		devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(attributes)}  
+	end
 end
