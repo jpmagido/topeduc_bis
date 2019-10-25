bd4da88a201d86fb08b972f201937c649a6487b0
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-10-25 17:41:07 +0200

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,17 @@ end
 def create_user
 
 	User.create(email: "jpmagido@me.com", password: "blablabla", first_name: "Jean-Philippe", last_name: "Magido", job: "Ruby Monster", nationality: "Française", phone: "0686631654", adress: "1 rue des Champs Elysées", ZIP_CODE: "75008", availability: true)
-	
+end
+
+def create_calendar
+	Calendar.create(user_id: 4, starts_at: "2019/10/26", duration: 3, recurring: false)
+
 end
 
 def perform
 	destroy_all
-	create_user
+	#create_user
+	create_calendar
 	puts "Seed done :)"
 end
 
