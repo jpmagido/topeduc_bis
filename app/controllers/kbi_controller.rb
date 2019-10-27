f7143a4bae82ccac5db6cdda63fef1afae01a446
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-10-27 19:47:25 +0100

    Config Kbis Views + Controller

--- app/controllers/kbi_controller.rb
+++ app/controllers/kbi_controller.rb
@@ -1,10 +1,34 @@
 class KbiController < ApplicationController
+  
   def new
+
+  	@new_kbis = Kbi.new
+  end
+
+  def create
+  	Kbi.create(params_kbis)
   end
 
   def edit
   end
 
-  def show
+  def update
   end
+
+  def show
+  	@last_kbis = Kbi.last.image_url
+  end
+
+  def destroy
+  	
+  end
+
+  private
+
+  def params_kbis
+
+  	params.require(:kbi).permit(:image, :user_id, :title)
+  	
+  end
+
 end
