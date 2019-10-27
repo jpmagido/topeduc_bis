5245b2cde62995896bdf1c7f6acdc911502b4380
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-10-27 19:08:07 +0100

    Config Carte Vitale Views + Controller

--- app/controllers/carte_vitale_controller.rb
+++ app/controllers/carte_vitale_controller.rb
@@ -1,10 +1,33 @@
 class CarteVitaleController < ApplicationController
+  
   def new
+
+  	@new_carte_vitale = CarteVitale.new
+  end
+
+  def create
+  	CarteVitale.create(params_carte_vitale)
   end
 
   def edit
   end
 
+  def update
+  end
+
   def show
+  	@last_carte_vitale = CarteVitale.last.image_url
+  end
+
+  def destroy
+  	
+  end
+
+  private
+
+  def params_carte_vitale
+
+  	params.require(:carte_vitale).permit(:image, :user_id, :title)
+  	
   end
 end
