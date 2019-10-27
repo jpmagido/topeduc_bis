5964b79e0aafa143b7a457952ad1701a0e2e52b9
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-10-27 19:15:42 +0100

    Config Diploma Views + Controller

--- app/controllers/diploma_controller.rb
+++ app/controllers/diploma_controller.rb
@@ -1,10 +1,32 @@
 class DiplomaController < ApplicationController
   def new
+
+  	@new_diploma = Diploma.new
+  end
+
+  def create
+  	Diploma.create(params_diploma)
   end
 
   def edit
   end
 
+  def update
+  end
+
   def show
+  	@last_diploma = Diploma.last.image_url
+  end
+
+  def destroy
+  	
+  end
+
+  private
+
+  def params_diploma
+
+  	params.require(:diploma).permit(:image, :user_id, :title)
+  	
   end
 end
