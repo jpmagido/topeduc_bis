18e65e5c8eff61a951242c1b774d5fe12e205693
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-10-27 19:59:21 +0100

    Config RIB Views + Controller

--- app/controllers/rib_controller.rb
+++ app/controllers/rib_controller.rb
@@ -1,10 +1,33 @@
 class RibController < ApplicationController
+  
   def new
+
+  	@new_rib = Rib.new
+  end
+
+  def create
+  	Rib.create(params_rib)
   end
 
   def edit
   end
 
+  def update
+  end
+
   def show
+  	@last_rib = Rib.last.image_url
+  end
+
+  def destroy
+  	
+  end
+
+  private
+
+  def params_rib
+
+  	params.require(:rib).permit(:image, :user_id, :title)
+  	
   end
 end
