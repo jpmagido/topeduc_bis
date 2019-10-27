190820e8920bb622f55484fbdcd01175df9fc4d6
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-10-27 19:25:53 +0100

    Config ID Views + Controller

--- app/controllers/id_controller.rb
+++ app/controllers/id_controller.rb
@@ -1,10 +1,32 @@
 class IdController < ApplicationController
+  
   def new
+  	@new_id = Id.new
+  end
+
+  def create
+  	Id.create(params_id)
   end
 
   def edit
   end
 
+  def update
+  end
+
   def show
+  	@last_id = Id.last.image_url
+  end
+
+  def destroy
+  	
+  end
+
+  private
+
+  def params_id
+
+  	params.require(:id).permit(:image, :user_id, :title, :recto)
+  	
   end
 end
