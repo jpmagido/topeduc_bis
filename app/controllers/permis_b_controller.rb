2c21d9dd629b67fc4844a5766963634983f23e27
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-10-27 19:47:39 +0100

    Config Permis_b Views + Controller

--- app/controllers/permis_b_controller.rb
+++ app/controllers/permis_b_controller.rb
@@ -1,10 +1,31 @@
 class PermisBController < ApplicationController
   def new
+  	@new_permis_b = PermisB.new
+  end
+
+  def create
+  	PermisB.create(params_permis_b)
   end
 
   def edit
   end
 
+  def update
+  end
+
   def show
+  	@last_permis_b = PermisB.last.image_url
+  end
+
+  def destroy
+  	
+  end
+
+  private
+
+  def params_permis_b
+
+  	params.require(:permis_b).permit(:image, :user_id, :title, :recto)
+  	
   end
 end
