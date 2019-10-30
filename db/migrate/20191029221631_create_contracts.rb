3e1e4ff3e4699b27ec059a4c803bd9098e8bae95
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-10-30 01:18:56 +0100

    Create Contract Model

---
+++ db/migrate/20191029221631_create_contracts.rb
@@ -0,0 +1,18 @@
+class CreateContracts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :contracts do |t|
+      t.belongs_to :user, index: true
+      t.belongs_to :request, index: true
+      t.datetime :starts_at
+      t.datetime :ends_at
+      t.integer :hourly_rate
+      t.string :adress
+      t.string :ZIP_CODE
+      t.string :client
+      t.string :job
+      t.text :comments
+
+      t.timestamps
+    end
+  end
+end
