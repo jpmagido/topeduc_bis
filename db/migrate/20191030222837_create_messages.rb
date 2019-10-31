ac90edf4784b6164ecc004fd1278d98004e5fee6
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-10-31 01:22:52 +0100

    Create Model Messages

--- db/migrate/20191030222837_create_messages.rb
+++ db/migrate/20191030222837_create_messages.rb
@@ -2,6 +2,10 @@ class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
       t.string :title
+      t.text :body
+      t.boolean :read?
+      t.references :sender, index: true
+      t.references :recipient, index: true
 
       t.timestamps
     end
