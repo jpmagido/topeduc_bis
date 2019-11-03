6b8272a1fa69ddf09fec3e63636e63d2d0457756
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-11-03 02:40:34 +0100

    update root path

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Rails.application.routes.draw do
 	get 'static/home'
 	get 'static/secret_page'
 	get 'static/test'
-	root 'static#test'
+	root 'static#home'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
